35f7c4059e1a43b38f9d077303c2dc79104870e8
Mikołaj Siedlarek <m.siedlarek@fatico.com>
2013-06-23 11:16:49 +0200

    no admin group

--- puppet/modules/user/manifests/virtual.pp
+++ puppet/modules/user/manifests/virtual.pp
@@ -22,7 +22,6 @@ class user::virtual {
       key      => 'AAAAB3NzaC1yc2EAAAADAQABAAACAQCvONPmMdTmHqiehwBnoEzZkjshKriZtpYs+qUu2vuSuZbxOjxo+/4g+DL5BfvSzt78/slDZ7ITzo7jdCG7/puQ5Ijl8OEiK10+kjUB/Tt5kp0dbmURKj2BnQbKYEOjIFVz9hEOKB/pvAuXj7uXcltyWO2UjLZOIyZT5lOAnSFcA9aTw1SnbYxGVROJqxHX49/l4ITqOH1Pz7WJoDngvt6RiUTRKv5zT/RvMfr62WG+QeXoCkT1O4cRHu83amIEOKt4vjKIYQy5aNNykEMepNXWn61IH8RLhbf7GNIfuBqJf4PuGNa9GU7uKpqSpeUkIDqXtpzaeCcjEwRvHUZeDMkI3F3TJsazVTdbqClSuABU/NYafLJNKQDW5a4TYysRG4wYUWtZ1TWve3uvEvsnSd2yCHiZe0H1ohPxmwjKkDornJqztsIuz+v/CnS2uletcHewDHPEUl0FgnqWXFhS+3MZ57/5VMhVfU8e/qa6goy7UXDnbCQ3eos2jmGukGy5fUafm+CppL+L9yURPPfcBWWHuN0B0ikvWigJVVaUd5koyyEvlpGg51PxWycfV4SLky1QLTNVlW/QF/A/fzTgej8+1+4s8A6yKa3GaS21pOpveGUeI2a/y29Z7hms1FE+FTAxtG7mAOB4oB7lngB2Oeslj6Y8OjN+vrc3V4saJlbJ4Q==',
       groups   => [
         'adm',
-        'admin',
         'sudo',
       ],
   }
